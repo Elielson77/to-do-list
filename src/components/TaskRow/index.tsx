393654a2964d412743e823a4695ff5317e2975c8
Elielson77 <elielsonll77@gmail.com>
2022-10-14 15:39:19 -0300

    add logic concluded

--- src/components/TaskRow/index.tsx
+++ src/components/TaskRow/index.tsx
@@ -8,56 +8,56 @@ import {
   IconButton,
 } from "@mui/material";
 import { Delete } from "@mui/icons-material";
-import { useState, useEffect } from "react";
 import { useTask } from "../../context/TaskContext";
+import styles from "./TaskRow.module.css";
 
 interface ITaskRowProps {
   name: string;
   description?: string;
   id: string;
+  concluded: boolean;
 }
 
-const TaskRow = ({ name, description, id }: ITaskRowProps) => {
+const TaskRow = ({ name, description, id, concluded }: ITaskRowProps) => {
   const theme = useTheme();
   const smUp = useMediaQuery(theme.breakpoints.up("sm"));
-  const [taskConcluded, setTaskConcluded] = useState<boolean>(false);
   const { concludedTask, noConcludedTask, removeTask } = useTask();
-
-  useEffect(() => {
-    taskConcluded && concludedTask(id);
-    !taskConcluded && noConcludedTask(id);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [concludedTask]);
 
   return (
     <Box
       sx={{
         textAlign: "center",
-        backgroundColor: "#fefefe",
+        backgroundColor: "#CFCFCF",
         width: smUp ? "30%" : "80%",
-        borderRadius: "2px",
+        borderRadius: "4px",
         display: "flex",
         justifyContent: "space-between",
         padding: "5px 10px",
-        borderBottom: "2px solid black",
+        borderBottom: "2px solid rgb(0, 0, 0, 0.54)",
       }}
+      className={concluded ? styles.concluded : ""}
     >
       <Box sx={{ display: "flex", alignItems: "center" }}>
         <Tooltip title="Marcar como concluída">
           <Checkbox
-            value={taskConcluded}
-            onChange={(e) => setTaskConcluded(e.currentTarget.checked)}
+            color="success"
+            checked={concluded}
+            onChange={(e) => {
+              e.currentTarget.checked ? concludedTask(id) : noConcludedTask(id);
+            }}
           />
         </Tooltip>
 
         <Tooltip title={description} arrow>
-          <Typography ml={1} sx={{ textTransform: "" }}>
+          <Typography
+            className={concluded ? styles.title_concluded : styles.title}
+          >
             {name[0].toUpperCase() + name.slice(1)}
           </Typography>
         </Tooltip>
       </Box>
-      <IconButton onClick={() => removeTask(id)} >
-        <Delete />
+      <IconButton onClick={() => removeTask(id)}>
+        <Delete color="error" />
       </IconButton>
     </Box>
   );
