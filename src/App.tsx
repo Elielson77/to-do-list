a2687c788fe082cf6da294f22844b3e51d1641a4
Elielson77 <elielsonll77@gmail.com>
2022-10-15 15:00:26 -0300

    add chips

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import {
   Box,
+  Chip,
   IconButton,
   Typography,
   useMediaQuery,
   useTheme,
 } from "@mui/material";
-import { AddCircleRounded } from "@mui/icons-material";
+import { AddCircleRounded, Done, Block, List } from "@mui/icons-material";
 import { useState, useContext } from "react";
 import CreateTaskModal from "./components/modals/CreateTaskModal";
 import TaskRow from "./components/TaskRow";
 import { TaskContext } from "./context/TaskContext";
 
+const filtersDefault = {
+  concluded: false,
+  nonConcluded: false,
+  all: true,
+};
+
 function App() {
   const theme = useTheme();
   const smUp = useMediaQuery(theme.breakpoints.up("sm"));
 
+  const [filters, setFilters] = useState(filtersDefault);
   const [openModalCreateTask, setOpenModalCreateTask] = useState(false);
   const { tasks } = useContext(TaskContext);
   const date = new Date();
@@ -24,6 +32,18 @@ function App() {
     month: "long",
     day: "numeric",
   });
+
+  const filterConcluded = () => {
+    setFilters({ concluded: true, nonConcluded: false, all: false });
+  };
+
+  const filterNonConcluded = () => {
+    setFilters({ concluded: false, nonConcluded: true, all: false });
+  };
+
+  const filterAll = () => {
+    setFilters({ concluded: false, nonConcluded: false, all: true });
+  };
 
   return (
     <Box sx={{ padding: "10px 16px" }}>
@@ -37,6 +57,27 @@ function App() {
       </Box>
       <br />
       <br />
+
+      <Box sx={{ display: "flex", gap: 1 }}>
+        <Chip
+          label="Concluidas"
+          sx={{ backgroundColor: "white" }}
+          icon={<Done />}
+          onClick={filterConcluded}
+        />
+        <Chip
+          label="Não concluidas"
+          sx={{ backgroundColor: "white" }}
+          icon={<Block />}
+          onClick={filterNonConcluded}
+        />
+        <Chip
+          label="Todas"
+          sx={{ backgroundColor: "white" }}
+          icon={<List />}
+          onClick={filterAll}
+        />
+      </Box>
 
       <Box
         sx={{
